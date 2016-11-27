64422cfb0251f2f8454fe2cfbf2ffb9a2f293247
Alex <peresok@mail.ru>
2016-11-27 17:12:59 +0300

    Телефонная книга
    Создание телефонной книги. Добавление в WebStorage записей телефонной
    книги, реализовано удаление. Необходимо добавить вывод данных в виде
    таблицы

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-
-
+function is_local_storage(){
+    try {
+        return 'localStorage' in window && window['localStorage']!==null;
+    }catch(e){
+        return false;
+    }
+}
+function save(){
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("tel1").value;
+    localStorage.setItem(name,phone);
+}
+function del_storage(){
+    if(confirm("Удалить все записи?")){
+    localStorage.clear();
+    alert("все записи удалены");
+    }else{
+        return;
+    }
+}
+function create_table(){
+    for (var i = 0; i < localStorage.length; ++i){
+        alert(localStorage.key(i));
+        alert(localStorage.getItem(localStorage.key(i)));
+        
+    }
+}
 
