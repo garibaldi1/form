e8a34dad535c1a1c2f4b260134d663170b726c71
Alex <peresok@mail.ru>
2016-12-17 12:47:04 +0300

    Эксперемент
    Добавлена функция создание таблицы в Б/Д. Но насколько это актуально???

--- Dip/add2.php
+++ Dip/add2.php
@@ -5,6 +5,8 @@ $header = $_POST['header'];
 $date = $_POST['date'];
 $place = $_POST['place'];
 $query = "INSERT INTO `vityaz`.`competition` (`id`, `competition`, `date`, `place`) VALUES (NULL, '$header', '$date', '$place');";
+$query2 = "CREATE TABLE `vityaz`.`table` (id SMALLINT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id), last_name INT, name INT, weigth INT, height INT);";
 $result = mysqli_query($link,$query);
+$result2 = mysqli_query($link,$query2);
 echo 'Событие создано';
 ?>
